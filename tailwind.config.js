01cee15374a166301eb4830c0d252c6b194ee9d2
DavidCs9 <davidcastrosi@hotmail.com>
2023-12-05 19:05:20 -0600

    home

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#D87D4A",
+        dark: "#141414",
+        lightgray: "#F1F1F1",
+        orange: "#D87D4A",
+      },
+    },
+  },
+  plugins: [],
+};
